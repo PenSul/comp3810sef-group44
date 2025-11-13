a27a8b8ab14bdf1284f08e2761ba88b6c11f446e
Jacky Chiang <68544952+PenSul@users.noreply.github.com>
2025-11-13 16:35:42 +0800

    Add new courses to seedCourses.js
    Added new courses to the seed data for the Computer Science program, including Software Engineering, Java Application Development, and Database Management.

--- scripts/seedCourses.js
+++ scripts/seedCourses.js
@@ -16,6 +16,45 @@ const courses = [
     semester: ['Autumn'],
     status: 'Active'
   },
+  {
+    courseCode: 'COMP3500SEF',
+    courseName: 'Software Engineering',
+    program: 'Computer Science',
+    credits: 3,
+    description: 'This course aims to introduce the concepts and applications of software engineering, explain their potential impacts on software productivity, quality assurance, cost and time to market during different software life cycles.',
+    level: 'Year 3',
+    prerequisites: [],
+    corequisites: [],
+    instructors: ['Dr. Ndudi Ezeamuzie'],
+    semester: ['Autumn'],
+    status: 'Active'
+  },
+  {
+    courseCode: 'COMP3120SEF',
+    courseName: 'Java Application Development',
+    program: 'Computer Science',
+    credits: 3,
+    description: 'This course aims to enable students to create maintainable software in Java to meet a great variety of computing requirements.',
+    level: 'Year 3',
+    prerequisites: [],
+    corequisites: [],
+    instructors: ['Dr. TSE Ka Wing'],
+    semester: ['Autumn'],
+    status: 'Active'
+  },
+  {
+    courseCode: 'COMP3200SEF',
+    courseName: 'Database Management',
+    program: 'Computer Science',
+    credits: 3,
+    description: 'This course describe essential principles and concepts of database management systems, perform data manipulation and extraction tasks effectively using SQL and Utilize the relational model to solve data modelling problems.',
+    level: 'Year 3',
+    prerequisites: [],
+    corequisites: [],
+    instructors: ['Dr. Wyman Wang'],
+    semester: ['Autumn'],
+    status: 'Active'
+  },
 ];
 
 async function seedCourses() {
